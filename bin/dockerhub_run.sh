934377ea3dec829a162c3237b705d11b6b9fc835
robinsonbarros <robinson.barros@outlook.com>
2022-10-29 09:08:28 -0300

    att_colors2

--- bin/dockerhub_run.sh
+++ bin/dockerhub_run.sh
@@ -1,7 +1,1 @@
-FILE=Gemfile.lock
-if [ -f "$FILE" ]; then
-    rm $FILE
-fi
-winpty docker run --rm -v "$PWD:/srv/jekyll/" -p "8080:8080" \
-                    -it amirpourmand/al-folio bundler  \
-                    exec jekyll serve --watch --port=8080 --host=0.0.0.0 
+docker run --rm -d -v $PWD":/srv/jekyll/" -p "8080:8080" -it amirpourmand/al-folio bundler exec jekyll serve --watch --port=8080 --host=0.0.0.0
